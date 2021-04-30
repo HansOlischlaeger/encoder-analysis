27707e10d19fdc38738f340c830dff0aa5ebe626
HansOlischlaeger <git@hans.olischlaeger.com>
2021-04-30 14:07:04 +0200

    Minor changes in tSNE.py and plots

--- tSNE.py
+++ tSNE.py
@@ -15,19 +15,22 @@ def makeNormalCluster(center, std, size):
 
 dim = 50
 size = 1000
-center = np.zeros(dim)
+center = np.ones(dim)
 center[0] = 1
+std = np.sqrt(np.random.uniform(0.1, 100, dim))
+std[0] = 0.1
 
-c1 = makeNormalCluster(center, 1, size)
-c2 = makeNormalCluster(-center, 1, size)
+c1 = makeNormalCluster(center, std, size)
+c2 = makeNormalCluster(-center, std, size)
 
 data = np.concatenate([c1, c2])
 
 # visualize latent representations with t-SNE with different parameters
-perplexities = np.arange(0, 5) * 15 + 10    # std is 30
+perplexities = np.arange(0, 7) * 15 + 10    # std is 30
 learning_rates = np.array([200])            # std is 200
 
 export_dir = "tSNE-visualization"
+forceOverride = True
 
 if not os.path.isdir(export_dir+"/pdf"):
     os.mkdir(export_dir+"/pdf")
@@ -37,7 +40,7 @@ if not os.path.isdir(export_dir+"/npy"):
 for p in perplexities:
     for lr in learning_rates:
         filename = f'tSNE_p={p}_lr={lr}.npy'
-        if filename not in os.listdir(export_dir+"/npy"):
+        if forceOverride or filename not in os.listdir(export_dir+"/npy"):
             np.save(f'{export_dir}/npy/{filename}', TSNE(n_components=2, perplexity=p, learning_rate=lr).fit_transform(data))
             print("Saved tSNE components in", filename)
         else:
@@ -51,11 +54,12 @@ for filename in os.listdir(export_dir+"/npy"):
         plt.suptitle("Visualization with t-SNE")
         plt.title(' '.join(s))
 
-        plt.scatter(data_embedded[:, 0][:size], data_embedded[:, 1][:size], alpha=0.5, color='C0', label='qcd')
-        plt.scatter(data_embedded[:, 0][size:], data_embedded[:, 1][size:], alpha=0.3, color='C3', label='top')
-        plt.show()
+        plt.scatter(data_embedded[:, 0][:size], data_embedded[:, 1][:size], alpha=0.5, color='C0', label='c1')
+        plt.scatter(data_embedded[:, 0][size:], data_embedded[:, 1][size:], alpha=0.3, color='C3', label='c2')
+        plt.legend()
         pdfpath = f'{export_dir}/pdf/{filename.split(".")[0]}.pdf'
         plt.savefig(pdfpath)
         print("Saved tSNE pdf in", pdfpath)
+        plt.show()
     else:
         print("unexpected file:", filename)
